e5be7a9f52111bae42fc92c91f14897543ac72c1
James Benner <james.benner@gmail.com>
2017-07-13 13:05:49 -0400

    Expanded tests.

--- spec/becquerel-spec.js
+++ spec/becquerel-spec.js
@@ -1,7 +1,12 @@
 const Bq = require('../');
+const http = require('http');
 const any = jasmine.any;
 
 describe('Bq', function () {
+    beforeEach(function () {
+        process.env['BQ_RUN_MSG'] = 'false';
+    });
+
     it('is a class/function', function () {
         expect(Bq).toEqual(any(Function));
     });
@@ -10,11 +15,19 @@ describe('Bq', function () {
         it('is a function', function () {
             expect(new Bq().route).toEqual(any(Function));
         });
+
+        it('returns a function', function () {
+            expect(new Bq().route()).toEqual(any(Function));
+        });
     });
 
     describe('.run', function () {
         it('is a function', function () {
             expect(new Bq().run).toEqual(any(Function));
+        });
+
+        it('returns an instance of `http.Server`', function () {
+            expect(new Bq().run()).toEqual(any(http.Server));
         });
     });
 
